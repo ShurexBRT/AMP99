beaa3a0da33b70551f89b2c125151531a3d43090
ShurexBRT <jovanovicpedja676@gmail.com>
2026-08-26 09:59:08 +0200

    chore: prepare alpha.13 release

--- scripts/build-store-msix.ps1
+++ scripts/build-store-msix.ps1
@@ -8,7 +8,7 @@ param(
     [Parameter(Mandatory = $true)]
     [string]$PublisherDisplayName,
 
-    [string]$Version = "0.2.0.12",
+    [string]$Version = "0.2.0.13",
 
     [ValidateSet("x64")]
     [string]$Architecture = "x64",
@@ -19,7 +19,7 @@ param(
 
     [string]$IconDir = "src-tauri/icons",
 
-    [string]$OutputPath = "src-tauri/target/release/bundle/msix/AMP99_0.2.0-alpha.12_x64.msix"
+    [string]$OutputPath = "src-tauri/target/release/bundle/msix/AMP99_0.2.0-alpha.13_x64.msix"
 )
 
 $ErrorActionPreference = "Stop"
